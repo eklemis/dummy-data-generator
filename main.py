ee44632adb69bf583c3bd7acd16ea6aa9b33606e
EKLEMIS SANTO NDUN <eklemis.ndun@gmail.com>
2023-08-23 10:59:37 +0800

    updated: May 2022

---
+++ main.py
@@ -0,0 +1,7 @@
+from lib.excel_translator import translate_content
+
+#translate_content("CHILDREN_ALL.xlsx",2, 13076, [6, 9, 10, 11, 12], 12)
+#translate_content("PERSON_ALL.xlsx",2, 115189, [7, 11, 12, 13, 14, 32,33, 34, 35,36, 37, 38, 39, 40, 41, 42], 14)
+#translate_content("MASTER_DONORS.xlsx",2, 18057, [3, 6, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 25, 26, 27, 29, 34], 14)
+#translate_content("List_of_enrolled_children.xlsx",2, 8915, [3, 7, 16, 17], 0)
+translate_content("DONOR_ALL.xlsx",2,38651,[6, 16, 17, 18, 19, 20, 22, 23], 0)
